85501b0520e31766936937717d7f2486310fe8da
makzuu <mr.greenmousth@gmail.com>
2024-03-05 22:27:11 -0300

    Add count letters

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ def count_words(text):
     words = text.split()
     return len(words)
 
+def count_letters(text):
+    letters_count = {}
+    for letter in text:
+        if letter.isalpha():
+            lower = letter.lower()
+            if lower in letters_count:
+                letters_count[lower] += 1
+            else:
+                letters_count[lower] = 1
+    return letters_count
+
 def main():
     path_to_file = "books/frankenstein.txt"
     with open(path_to_file) as f:
@@ -9,5 +20,8 @@ def main():
 
     words_count = count_words(file_contents)
     print(f"words: {words_count}")
+
+    letters_count = count_letters(file_contents)
+    print(letters_count)
 
 main()
