258d2ba7dff3adb9c87087a7a12e02218b3659cd
makzuu <mr.greenmousth@gmail.com>
2024-03-05 23:04:41 -0300

    Add print report

--- main.py
+++ main.py
@@ -13,15 +13,26 @@ def count_letters(text):
                 letters_count[lower] = 1
     return letters_count
 
+def sort_on(dict):
+    return dict["count"]
+
 def main():
     path_to_file = "books/frankenstein.txt"
     with open(path_to_file) as f:
         file_contents = f.read()
 
     words_count = count_words(file_contents)
-    print(f"words: {words_count}")
 
     letters_count = count_letters(file_contents)
-    print(letters_count)
+    letters_count_list = []
+    for letter in letters_count:
+        letters_count_list.append({ "letter": letter, "count": letters_count[letter] })
+    letters_count_list.sort(key=sort_on, reverse=True)
+
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{words_count} words found in the document")
+    for dict in letters_count_list:
+        print(f"The '{dict["letter"]}' character was found {dict["count"]} times")
+    print("--- End report ---")
 
 main()
